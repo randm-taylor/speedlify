0f53d1ff56bcaf4348f7140cdbc2482595d99021
Taylor Edginton <taylor@reynoldsandmyers.com>
2025-12-16 09:00:24 -0700

    Unignore data and template mod.

---
+++ _data/sites/bc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	name: "Boise Cascade", // optional, falls back to object key
+	description: "https://www.bc.com",
+	options: {
+		runs: 1,
+		frequency: 1, // (in minutes)
+	},
+	urls: [
+		"https://www.bc.com/",
+		"https://www.bc.com/careers/",
+		"https://www.bc.com/span-and-size-charts-for-lvl-bci-joists/",
+		"https://www.bc.com/ewp/versa-lam-lvl/",
+		"https://www.bc.com/ewp/matters/",
+		"https://www.bc.com/ewp/software/bc-calc/",
+		"https://www.bc.com/locations/",
+		"https://www.bc.com/ewp/bci-joists/",
+		"https://www.bc.com/ewp/boise-glulam/",
+		"https://www.bc.com/contact/"
+	]
+};
