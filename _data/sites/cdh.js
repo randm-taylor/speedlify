64fd27a04c4818c709563f2ee557a1b7d6cf9be3
Taylor Edginton <taylor@reynoldsandmyers.com>
2026-04-07 10:38:27 -0600

    Add CDH

---
+++ _data/sites/cdh.js
@@ -0,0 +1,19 @@
+module.exports = {
+	name: "CDH", // optional, falls back to object key
+	description: "https://cdh.idaho.gov",
+	options: {
+		runs: 1,
+		frequency: 1, // (in minutes)
+	},
+	urls: [
+		"https://cdh.idaho.gov/",
+		"https://cdh.idaho.gov/licenses-permits-inspections/food-establishments/",
+		"https://cdh.idaho.gov/licenses-permits-inspections/water-septic-subdivisions/",
+		"https://cdh.idaho.gov/licenses-permits-inspections/",
+		"https://cdh.idaho.gov/shop/",
+		"https://cdh.idaho.gov/support-programs/women-infants-children/",
+		"https://cdh.idaho.gov/about/contact/",
+		"https://cdh.idaho.gov/lawful-presence-attestation-english/",
+		"https://cdh.idaho.gov/healthcare-appointments/immunizations/"
+	]
+};
